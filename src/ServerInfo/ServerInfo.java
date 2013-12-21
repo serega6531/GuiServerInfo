6cb8f69d8faf1d7bf6933de67a29da7003aac461
«serega6531» <serega6531@yandex.ru>
2013-12-21 15:55:29 +0400

    Add colors

--- src/ServerInfo/ServerInfo.java
+++ src/ServerInfo/ServerInfo.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Logger;
 
+import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -21,6 +22,10 @@ public class ServerInfo extends JavaPlugin {
 	
 	Logger log;
 	List<WrappedGameProfile> Text = new ArrayList<WrappedGameProfile>();
+	
+	private String ConvertFormat(String format){
+	    return format.replace("#0", ""+ChatColor.BLACK).replace("#1", ""+ChatColor.DARK_BLUE).replace("#2", ""+ChatColor.DARK_GREEN).replace("#3", ""+ChatColor.DARK_AQUA).replace("#4", ""+ChatColor.DARK_RED).replace("#5", ""+ChatColor.DARK_PURPLE).replace("#6", ""+ChatColor.GOLD).replace("#7", ""+ChatColor.GRAY).replace("#8", ""+ChatColor.DARK_GRAY).replace("#9", ""+ChatColor.BLUE).replace("#a", ""+ChatColor.GREEN).replace("#b", ""+ChatColor.AQUA).replace("#c", ""+ChatColor.RED).replace("#d", ""+ChatColor.LIGHT_PURPLE).replace("#e", ""+ChatColor.YELLOW).replace("#f", ""+ChatColor.WHITE);
+	}
 	
 	
 	public void onEnable(){
@@ -41,7 +46,7 @@ public class ServerInfo extends JavaPlugin {
 			Text.add(
 					new WrappedGameProfile(
 							"id" + i + 1,
-							config.getStringList("Text").get(i))
+							ConvertFormat(config.getStringList("Text").get(i)))
 					);
 			}
 		log.info("GuiServerInfo active!");
